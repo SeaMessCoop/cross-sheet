d566f539cf6a07dc7121a3e7aeb9537c8af66122
Steve Geluso <stevegeluso@gmail.com>
2018-06-21 12:19:38 -0700

    preventing overwriting existing values

--- main.py
+++ main.py
@@ -5,15 +5,29 @@ with open(filepath) as csvfile:
     rows = csv.reader(csvfile)
     rows = list(rows)
     header = rows[0]
-    output_rows = [header]
-    for (rowi, row) in enumerate(rows):
+    output_rows = []
+    for (irow, row) in enumerate(rows):
         output_row = []
-        for (coli, col) in enumerate(header):
+        for (icol, col) in enumerate(header):
             price1 = row[0]
-            price2 = header[coli]
+            price2 = header[icol]
 
+            # prevent overwriting anything that's already been set
+            if (row[icol]):
+                print('value present', row[icol])
+                output_row.append(row[icol])
+                continue
+
+            # if the row and column are the same we're
+            # going to the same place so just put the
+            # one base price.
+            if (irow == icol):
+                output_row.append(price1)
+                continue
+
+            # ignore anything that doesn't have a price set
             if (price1 is '' or price2 is ''):
-                output_row += ','
+                output_row.append('')
                 continue
 
             price1 = float(price1)
